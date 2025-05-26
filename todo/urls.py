cfb6422210dd0b73c06b260b05179a2cdb77111b
ChandraShekhar <csyadav0513@gmail.com>
2025-05-26 12:13:02 +0530

    Add ToggleTodoView URL pattern for toggling todo completion status

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TodoListCreateView, TodoDetailView
+from .views import TodoListCreateView, TodoDetailView, ToggleTodoView
 
 urlpatterns = [
     path('todos/', TodoListCreateView.as_view(), name='todo-list-create'),
     path('todos/<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
+    path('todos/<int:pk>/toggle/',
+         ToggleTodoView.as_view(), name='toggle_todo'),
 
 ]
