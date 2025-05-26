d5348bae1922a0fb96b79866fefdfd03334ac2ef
ChandraShekhar <csyadav0513@gmail.com>
2025-05-26 12:42:08 +0530

    add IsAuthenticated

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Todo
-from django.shortcuts import get_object_or_404
 from .serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -14,18 +12,21 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 #! List all todos, create a new todo
 class TodoListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
 
 #! Retrieve, update or delete one todo by id
 class TodoDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 
 #! Toggle Todo
 class ToggleTodoView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request, pk):
         todo = get_object_or_404(Todo, pk=pk)
         
