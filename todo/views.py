42784868a6a37c3a30c028e098672bb97e630205
ChandraShekhar <csyadav0513@gmail.com>
2025-05-26 11:21:15 +0530

    Add TodoListCreateView and TodoDetailView for managing todos

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
+
+
+# List all todos, create a new todo
+class TodoListCreateView(ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+# Retrieve, update or delete one todo by id
+
+
+class TodoDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
