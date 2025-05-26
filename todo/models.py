1736f9e045fcf7d37a8a32b5a2d5705e8d07389e
ChandraShekhar <csyadav0513@gmail.com>
2025-05-26 10:26:19 +0530

    Refactor Todo model structure and formatting

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=300)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["-created_at"]
+
