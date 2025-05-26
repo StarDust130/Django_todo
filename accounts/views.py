7262ccf1d4a18e21b52abd0723c1aa577190b276
ChandraShekhar <csyadav0513@gmail.com>
2025-05-26 12:45:32 +0530

    make view better

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,11 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
-from rest_framework.serializers import ModelSerializer
+from .serializer import UserSerializer
 
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
-        return user
-
-
+#! Create Account
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)  # Anyone can register
